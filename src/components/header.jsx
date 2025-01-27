3030ea28abdb1a5dc7b286f350a6e8f09dd32ff0
Pratik <pratiksolanki2026@gmail.com>
2025-01-27 11:11:16 +0530

    Made the header responsive

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,13 +14,14 @@ import {
   Text,
   Button,
   Select,
-  Paper,
+  Box,
 } from "@mantine/core";
+// import { useQueryClient } from "@tanstack/react-query";
 import PropTypes from "prop-types";
-import { notifications } from "@mantine/notifications";
 import { setRole, setCurrentAccessibleModules } from "../redux/userslice";
 import classes from "../Modules/Dashboard/Dashboard.module.css";
 import avatarImage from "../assets/avatar.png";
+
 import { logoutRoute, updateRoleRoute } from "../routes/dashboardRoutes";
 
 function Header({ opened, toggleSidebar }) {
@@ -30,6 +31,7 @@ function Header({ opened, toggleSidebar }) {
   const role = useSelector((state) => state.user.role);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  // const queryclient = useQueryClient();
 
   const handleRoleChange = async (newRole) => {
     const token = localStorage.getItem("authToken");
@@ -45,19 +47,6 @@ function Header({ opened, toggleSidebar }) {
           },
         },
       );
-
-      notifications.show({
-        title: "Role Updated",
-        message: (
-          <Flex gap="4px">
-            <Text fz="sm">Your role has been changed to </Text>
-            <Text fz="sm" fw="500" c="dark">
-              {newRole}
-            </Text>
-          </Flex>
-        ),
-        color: "green",
-      });
       console.log(response.data.message);
       dispatch(setRole(newRole));
       dispatch(setCurrentAccessibleModules());
@@ -82,6 +71,7 @@ function Header({ opened, toggleSidebar }) {
       );
       localStorage.removeItem("authToken");
       navigate("/accounts/login");
+      // queryclient.invalidateQueries();
       console.log("User logged out successfully");
     } catch (err) {
       console.error("Logout error:", err);
@@ -89,21 +79,25 @@ function Header({ opened, toggleSidebar }) {
   };
 
   return (
-    <Paper
+    <Box
       bg="#F5F7F8"
-      justify={{ base: "space-between" }}
+      justify="space-between"
       align="center"
       pl="sm"
       h="64px" // Height has already been set in layout.jsx but had to set the height here as well for properly aligning the avatar
     >
-      <Burger
-        opened={opened}
-        onClick={toggleSidebar}
-        hiddenFrom="sm"
-        size="sm"
-      />
-      <Flex justify="space-between" align="center" h="100%">
-        <Text fz="h2">FUSION - IIITDMJ's ERP Portal</Text>
+      <Flex justify={{ base: "space-between" }} align="center" h="100%">
+        <Box>
+          <Burger
+            opened={opened}
+            onClick={toggleSidebar}
+            hiddenFrom="sm"
+            size="sm"
+          />
+        </Box>
+        <Text fz="h2" visibleFrom="md">
+          FUSION - IIITDMJ's ERP Portal
+        </Text>
         <Flex
           justify="flex-end"
           align="center"
@@ -121,8 +115,6 @@ function Header({ opened, toggleSidebar }) {
             value={role}
             onChange={handleRoleChange}
             placeholder="Role"
-            mr="64px"
-            size="md"
           />
           <Indicator>
             <Bell color="orange" size="32px" cursor="pointer" />
@@ -186,7 +178,7 @@ function Header({ opened, toggleSidebar }) {
           </Popover>
         </Flex>
       </Flex>
-    </Paper>
+    </Box>
   );
 }
 
